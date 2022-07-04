c2fd5c2eb3bf3bdb74c8740d2ab36370741ee3cc
Swaubhik Chakraborty <b18csel299@cit.ac.in>
2022-07-04 23:53:50 +0530

    Update Layout.vue
    Imported LatestIFO component

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -2,7 +2,7 @@
     <div class="layout">
         <Header />
         <main>
-            <slot />
+            <LatestIFO />
         </main>
         <Footer />
     </div>
@@ -11,27 +11,88 @@
 <script>
 import Header from './Header.vue';
 import Footer from './Footer.vue';
+import LatestIFO from './LatestIFO.vue';
 export default {
-    components: { Header, Footer }
+    components: { Header, Footer, LatestIFO }
 }
 </script>
 
-<style>
+<style lang="scss">
 @import url('https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700&family=Oxygen:wght@700&display=swap');
 
 :root {
     --color-primary: rgba(0, 232, 162, 1);
     --color-secondary: rgba(0, 173, 232, 1);
     margin: 0;
-    padding: 0 2rem;
+    padding: 0 8rem;
     box-sizing: border-box;
     font-family: 'Nunito', sans-serif;
+    font-style: normal;
     background-color: black;
     color: white;
+
+    /* mobile */
+    @media (max-width: 768px) {
+        padding: 0;
+    }
 }
 
 a {
     color: inherit;
     text-decoration: inherit;
+}
+
+.btn {
+    padding: 1rem 2rem;
+    color: #FFFFFF;
+    background: rgba(0, 0, 0, 0.5);
+    border: 1px solid #FFFFFF;
+    backdrop-filter: blur(1px);
+    border-radius: 10px;
+}
+
+.btn-primary {
+    background-color: var(--color-primary);
+    border-radius: 10px;
+    border-color: var(--color-primary);
+    padding: 1rem 2rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.icon {
+    height: 1.2rem;
+    width: 3rem;
+    margin-left: 1rem;
+}
+
+.text-primary {
+    color: var(--color-primary);
+}
+
+.mt-2 {
+    margin-top: 2rem;
+}
+
+.bold {
+    font-weight: 700;
+}
+
+.semibold {
+    font-weight: 600;
+}
+
+.light {
+    font-weight: 300;
+    font-size: 0.8rem;
+}
+
+@media (max-width: 768px) {
+
+    .btn {
+        margin-bottom: 2rem;
+        padding: 1rem 3.5rem;
+    }
 }
 </style>
