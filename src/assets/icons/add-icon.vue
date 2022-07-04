f593cbaa21361f62944e3f4172145867f2b27957
Swaubhik Chakraborty <b18csel299@cit.ac.in>
2022-07-04 23:52:09 +0530

    Importing assets
    Imported addIcon, stopwatchIcon and background for latest IFO

---
+++ src/assets/icons/add-icon.vue
@@ -0,0 +1,17 @@
+<template>
+    <svg width="36" height="31" viewBox="0 0 36 31" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <line x1="17.4893" y1="29.7695" x2="17.4893" y2="0.769531" stroke="black" stroke-linecap="round" />
+        <line x1="0.5" y1="-0.5" x2="33.7935" y2="-0.5" transform="matrix(1 0 0 -1 0.84256 15.2695)" stroke="black"
+            stroke-linecap="round" />
+    </svg>
+
+</template>
+
+<script>
+export default {
+    name: 'addIcon',
+}
+</script>
+
+<style>
+</style>
---
+++ src/assets/icons/stop-watch.vue
@@ -0,0 +1,20 @@
+<template>
+    <svg width="60" height="70" viewBox="0 0 60 70" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="29.7292" cy="40.3216" r="28.6589" stroke="#00E8A2" stroke-width="2" />
+        <circle cx="29.7309" cy="40.3218" r="22.1719" stroke="white" />
+        <circle r="6.19129" transform="matrix(-1 -8.74228e-08 -8.74228e-08 1 29.7296 40.3219)" stroke="#00E8A2"
+            stroke-width="2" />
+        <rect x="28.1519" y="15.6143" width="3.15346" height="18.9699" rx="1.57673" fill="#00E8A2" />
+        <rect x="26.2475" y="7.36023" width="6.96621" height="3.94558" fill="#00E8A2" />
+        <rect x="20.716" y="0.980469" width="18.0279" height="5.42794" rx="2.71397" fill="#00E8A2" />
+    </svg>
+</template>
+
+<script>
+export default {
+    name: 'StopWatch',
+}
+</script>
+
+<style>
+</style>
