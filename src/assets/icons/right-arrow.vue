22d121a9a4150b4ad40b49f9bb7800bfd1d5f96a
Swaubhik Chakraborty <b18csel299@cit.ac.in>
2022-07-04 18:41:07 +0530

    Create right-arrow.vue
    Added svg for the right chevron

---
+++ src/assets/icons/right-arrow.vue
@@ -0,0 +1,16 @@
+<template>
+    <svg width="60" height="27" viewBox="0 0 60 27" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <line x1="59.1835" y1="13.6055" x2="46.6187" y2="1.04067" stroke="black" stroke-linecap="round" />
+        <line x1="0.5" y1="-0.5" x2="18.2693" y2="-0.5"
+            transform="matrix(0.707107 -0.707107 -0.707107 -0.707107 45.9844 26.3594)" stroke="black"
+            stroke-linecap="round" />
+        <line x1="59.2227" y1="14.3086" x2="0.297409" y2="14.3086" stroke="black" />
+    </svg>
+
+</template>
+
+<script>
+export default {
+    name: "RightArrow",
+}
+</script>
