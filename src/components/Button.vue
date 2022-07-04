7502a14b60eaa40f3f99b9e3a8de83b8b11840c9
Swaubhik Chakraborty <b18csel299@cit.ac.in>
2022-07-04 23:52:27 +0530

    Create Button.vue
    Created reusable button component

---
+++ src/components/Button.vue
@@ -0,0 +1,17 @@
+<template>
+    <button>
+        {{ msg }}
+        <slot />
+    </button>
+</template>
+<script>
+
+export default {
+    name: "Button",
+    props: {
+        msg: String,
+    },
+};
+</script>
+<style lang="scss">
+</style>
